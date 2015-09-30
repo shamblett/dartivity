fc7c30cecbfde01791ae51cfe88af7ebea1b7658
Steve Hamblett <steve.hamblett@linux.com>
2015-09-30 15:51:12 +0100

    Add combined messaging/iotivity client test

---
+++ test/dartivity_test_client.dart
@@ -0,0 +1,46 @@
+/*
+ * Package : dartivity
+ * Author : S. Hamblett <steve.hamblett@linux.com>
+ * Date   : 28/09/2015
+ * Copyright :  S.Hamblett 2015
+ */
+
+library dartivity.test;
+
+import 'dart:async';
+import 'dart:io';
+
+import 'package:dartivity/dartivity.dart';
+
+Future main() async {
+  final int badExitCode = -1;
+
+  // Instantiate a Dartivity client and initialise for
+  // both messaging and iotivity, ie a normal client configuration.
+  Dartivity dartivity = new Dartivity(Mode.both);
+
+  DartivityIotivityCfg iotCfg = new DartivityIotivityCfg(
+      qos: DartivityIotivityCfg.QualityOfService_LowQos);
+
+  await dartivity.initialise(
+      DartivityCfg.MESS_CRED_PATH, DartivityCfg.MESS_PROJECT_ID, iotCfg);
+
+  if (dartivity.initialised) {
+    print("Initialse Status is true - OK");
+  } else {
+    print("Oops Initialse Status is false - ERROR");
+    exit(badExitCode);
+  }
+
+
+  // Message monitoring
+  var subscription = dartivity.nextMessage.listen((DartivityMessage message) {
+    print("Message received ${message.toString()}");
+
+  });
+
+  // Close down
+  //dartivity.close();
+
+
+}
